e6008836ec2e5460b5479c48485d22ae98bf769d
Diego R <diego.riveros.g@mail.pucv.cl>
2024-02-19 18:15:05 -0300

    Archivos

---
+++ Aidofobia.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	//Ejercicio de palindromo clasico
+
+	return 0
+}
---
+++ SplitStrings.js
@@ -0,0 +1,21 @@
+solution('abcdefg')
+
+function solution(str){
+    let stringPar = []
+    var contPar = 0
+   var cont = 0
+   var i = 0
+   for(i = 0; i < str.length; i++){
+    cont++
+    if(cont === 2){
+        stringPar[contPar] = str[i-1] + str[i]
+        contPar++
+      cont = 0;
+    }
+
+   }
+   if(str.length % 2 !== 0){
+       stringPar[contPar] = str[str.length -1] + '_'
+   }
+   console.log(JSON.stringify(stringPar))
+}
